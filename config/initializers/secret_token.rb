e714bc5446b376bf4ffa0ee0df85372340db8748
Tiffany Lu <tweilu@mit.edu>
2014-04-24 00:55:29 -0400

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pettracker::Application.config.secret_key_base = 'd73ded2a19f10842359553a7310d80c4c266d14c3c1e64055956cd8c781f48b09ddda90ee06bb9bad7f774868f3fcd51412ffa5bff80d154bd5cdfc595dd38ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pettracker::Application.config.session_store :cookie_store, key: '_pettracker_session'
