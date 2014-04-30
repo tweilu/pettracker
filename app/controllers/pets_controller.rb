2444ea80f5879476e2b1e4e07afdba008c8a97bf
Tiffany Lu <tweilu@mit.edu>
2014-04-29 20:09:10 -0400

    can add to my pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,25 @@ class PetsController < ApplicationController
   end
 
   def index
-  	@pets = current_user.my_pets
+    @pets = current_user.my_pets
+    @newpet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.owner_id = current_user.id
+    if @pet.save
+      respond_to do |format|
+        format.js
+      end
+    else
+      render 'new'
+    end
   end
 
   def sitting
-  	@pets = current_user.sitting_pets
+    @pets = current_user.sitting_pets
   end
-
 
   private
 
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ActiveRecord::Base
+	validates :name, presence: true
+	validates :pet_type, presence: true
 	belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
 	belongs_to :sitter, :class_name => 'User', :foreign_key => 'sitter_id'
 
