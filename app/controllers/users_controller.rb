cf5e4c13c1f6ba75d0476a07b2bd2443324d37cb
Tiffany Lu <tweilu@mit.edu>
2014-05-09 02:16:01 -0400

    welcome message for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      flash[:success] = "Welcome to PawPro!"
+      redirect_to pets_path
     else
       render 'new'
     end
