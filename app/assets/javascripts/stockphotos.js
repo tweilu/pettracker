84951f4fcad4782e657052cadd9ae984b72b5a6c
Tiffany Lu <tweilu@mit.edu>
2014-05-09 02:32:15 -0400

    y heroku

--- app/assets/javascripts/stockphotos.js
+++ app/assets/javascripts/stockphotos.js
@@ -2,6 +2,10 @@
 
 var petphotolist = ["bird1.jpg",  "bunny1.jpg",  "cat1.jpg",  "cat2.jpg",  "cat3.jpg", "cat4.jpg", "cat5.jpg", "cat6.jpg", "cuttlefish1.jpg",  "dog1.jpg",  "dog2.jpg",  "dog3.jpg", "dog4.jpg", "dog5.jpg", "dog6.jpg", "dog7.jpg", "hedgehog1.jpg", "hedgehog2.jpg", "pig1.jpg", "seahorse1.jpg", "sloth1.jpg", "sloth2.jpg", "sloth3.jpg",  "hamster1.jpg",  "turtle1.jpg"];
 
+var hashCode = function(s){
+      return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
 function getRandomPet() {
     return "/assets/stockphotos/" + petphotolist[Math.floor(Math.random()*petphotolist.length)];
 }
@@ -26,10 +30,6 @@ function addSpecificPet(src) {
     $("#photos").append(div);
 }
 
-
-hashCode = function(s){
-      return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
-}
 
 
 function getNonrandomPet(s) {
