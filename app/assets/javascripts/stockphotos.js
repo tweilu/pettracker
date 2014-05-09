c9993707cd1c3d181be547cc2a93b2955ed696b4
Tiffany Lu <tweilu@mit.edu>
2014-05-09 02:34:55 -0400

    what is wrong with this hashcode function

--- app/assets/javascripts/stockphotos.js
+++ app/assets/javascripts/stockphotos.js
@@ -2,7 +2,7 @@
 
 var petphotolist = ["bird1.jpg",  "bunny1.jpg",  "cat1.jpg",  "cat2.jpg",  "cat3.jpg", "cat4.jpg", "cat5.jpg", "cat6.jpg", "cuttlefish1.jpg",  "dog1.jpg",  "dog2.jpg",  "dog3.jpg", "dog4.jpg", "dog5.jpg", "dog6.jpg", "dog7.jpg", "hedgehog1.jpg", "hedgehog2.jpg", "pig1.jpg", "seahorse1.jpg", "sloth1.jpg", "sloth2.jpg", "sloth3.jpg",  "hamster1.jpg",  "turtle1.jpg"];
 
-var hashCode = function(s){
+function hashCode(s){
       return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
 }
 
